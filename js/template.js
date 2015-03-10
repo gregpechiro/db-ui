0a204424081636800c497c5d077a872e43864931
gregpechiro <gregpechiro@yahoo.com>
2015-03-10 17:03:04 -0400

    template creation

--- js/template.js
+++ js/template.js
@@ -1,6 +1,33 @@
 var row = 0;
 var position = 0;
 var currentWidth = 0;
+var templates = [];
+var resource = "data/templates.json";
+
+
+function getUrlParameter(param) {
+    var pageURL = window.location.search.substring(1);
+    var urlVariables = pageURL.split('&');
+    for (var i = 0; i < urlVariables.length; i++) {
+        var parameterName = urlVariables[i].split('=');
+        if (parameterName[0] == param) {
+            return parameterName[1];
+        }
+    }
+}
+
+function getData(url, handleData) {
+	$.ajax({
+		url: url,
+		dataType:'json',
+		success:function(data) {
+			handleData(data);
+		},
+		error: function() {
+			console.log('error retrieving');
+		}
+	});
+}
 
 
 function getRowWidth(id) {
@@ -15,7 +42,7 @@ function getRowWidth(id) {
 
 function addRow() {
     row++;
-    $('div[id="positions"]').html($('div[id="positions"]').html() + '<div id="row' + row + '" class="units-row units-padding"></div>');
+    $('div[id="positions"]').append('<div id="row' + row + '" class="units-row units-padding myRow"></div>');
     $('select[id="chosenRow"]').append('<option id="' + row + '" value="' + row + '">Row ' + row + '</option>');
 }
 
@@ -24,9 +51,8 @@ function delRow(id) {
     $('select[id="chosenRow"] option[id="' + id + '"]').remove();
 }
 
-function addPosition() {
+function addPosition(width) {
     position++;
-    var width =+ $('input[id="width"]').val();
     if (currentWidth + width > 100) {
         addRow();
         currentWidth = width;
@@ -40,7 +66,7 @@ function addPosition() {
 function delPosition() {
     var width =+ $('div[id="position' + position + '"]').attr('data-width');
     $('div[id="position' + position + '"]').remove();
-    if (currentWidth - width < 1 && row > 1) {
+    if ((currentWidth - width) < 1 && row > 1) {
         delRow(row);
         row--;
         currentWidth = getRowWidth(row);
@@ -52,24 +78,73 @@ function delPosition() {
 }
 
 function addRowPosition(id) {
-    var width = getRowWidth(id);
+    var rowWidth = getRowWidth(id);
     position++;
-    var rowWidth =+ $('input[id="rowWidth"]').val();
+    var width =+ $('input[id="rowWidth"]').val();
     if (width + rowWidth <= 100) {
         $('div[id="row' + id + '"]').append('<div id="position' + position + '" style="border:1px solid black" data-width="' + width + '" class="position unit-' + width + '">');
         $('div[id="position' + position + '"]').html('<h3>Position ' + position + '</h3>');
+        reorganize();
     } else {
         alert('New position too wide for row');
     }
 }
 
+function delRowPosition(id) {
+    var positions = $('div[id="row' + id + '"] div.position');
+    positions[positions.length - 1].remove();
+    if (getRowWidth(id) < 1) {
+        delRow(id);
+    }
+    reorganize();
+}
+
+function reorganize() {
+    position = 0;
+    row = 0;
+    currentWidth = 0;
+    $('select[id="chosenRow"]').html('<option value="none">Choose a row</option>');
+    var positions = $('div.position')
+    $('div[id="positions"]').html('');
+    addRow();
+    for (var i = 0; i < positions.length; i++) {
+        var width =+ positions[i].getAttribute('data-width');
+        addPosition(width);
+    }
+}
+
+function save() {
+    var template = {};
+    template["name"] = $('input[id="templateName"]').val();
+    var positions = [];
+    for (var i = 0; i < position; i++) {
+        var w =+ $('div[id="position' + (i+1) + '"]').attr('data-width');
+        positions.push(w);
+    }
+    template['positions'] = positions;
+    alert(JSON.stringify(template));
+}
+
+function fill(id) {
+    var template = templates[id];
+    $('input[id="templateName"]').val(template.name);
+    for (var i = 0; i < template.positions.length; i++) {
+        addPosition(template.positions[i]);
+    }
+}
+
 $(document).ready(function() {
     $('button[id="addPosition"]').click(function() {
-        addPosition();
+        var width =+ $('input[id="width"]').val();
+        addPosition(width);
     });
 
-    $('button[id="delPosition"]').click(function() {
-        delPosition();
+    // $('button[id="delPosition"]').click(function() {
+    //     delPosition();
+    // });
+
+    $('button[id="save"]').click(function() {
+        save();
     });
 
     $('button[id="addRowPosition"]').click(function() {
@@ -79,6 +154,22 @@ $(document).ready(function() {
         }
     });
 
+    $('button[id="delRowPosition"]').click(function() {
+        var id = $('select[id="chosenRow"]').val();
+        if (id != 'none') {
+            delRowPosition(id);
+        }
+    });
+
     addRow();
+
+    getData(resource, function(data) {
+		templates = data;
+		var tempId = getUrlParameter('template');
+		if (tempId != null) {
+			tempId =+ tempId;
+			fill(tempId);
+		}
+	});
 
 });
